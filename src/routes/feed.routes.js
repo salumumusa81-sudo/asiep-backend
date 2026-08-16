c614eaf0d435b931672d70ae2a70d8a4cce33a0a
Wakala <salumumusa81@gmail.com>
2026-08-16 20:28:19 +0300

    Fix feed routes - remove duplicate import

--- src/routes/feed.routes.js
+++ src/routes/feed.routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { protect } = require('../middleware/auth.middleware');
-const { getFeed, createPost, toggleLike, addComment } = require('../controllers/feed.controller');
 const { getFeed, createPost, toggleLike, addComment, deletePost } = require('../controllers/feed.controller');
 
 router.get('/',                    protect, getFeed);
