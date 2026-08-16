29b3dcc8069ea63dfcb7fd55db43a4b661aafd95
Wakala <salumumusa81@gmail.com>
2026-08-16 20:20:44 +0300

    Add delete post route to feed

--- src/routes/feed.routes.js
+++ src/routes/feed.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const { protect } = require('../middleware/auth.middleware');
 const { getFeed, createPost, toggleLike, addComment } = require('../controllers/feed.controller');
+const { getFeed, createPost, toggleLike, addComment, deletePost } = require('../controllers/feed.controller');
 
 router.get('/',                    protect, getFeed);
 router.post('/',                   protect, createPost);
 router.post('/:postId/like',       protect, toggleLike);
 router.post('/:postId/comment',    protect, addComment);
+router.delete('/:postId', protect, deletePost);
 
 module.exports = router;
