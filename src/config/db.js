1a33109e825d024c4601e83fff7a286cfe422107
Wakala <salumumusa81@gmail.com>
2026-09-03 10:44:00 +0300

    Keep database connection alive

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,19 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
-  log: ['query', 'error'],
+  log: ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
+
+// Keep database connection alive
+setInterval(async () => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+  } catch (e) {}
+}, 4 * 60 * 1000); // Every 4 minutes
 
 module.exports = prisma;
