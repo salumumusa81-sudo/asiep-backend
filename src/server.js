7dbd5d98f7285321419c8fbd3b821f8d1787240e
Wakala <salumumusa81@gmail.com>
2026-09-03 09:29:36 +0300

    Add error logging

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const app = require('./app');
-
 const PORT = process.env.PORT || 5000;
+
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err);
+});
 
 app.listen(PORT, () => {
   console.log(`\n🌍 ASIEP Backend running on port ${PORT}`);
